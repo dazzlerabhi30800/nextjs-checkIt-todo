c10c3357fc5f0989a38d90249c6f7806d53df4df
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-03-11 21:01:48 +0530

    removed ununsed useEffect from the infocomp

--- app/_components/InfoComp.tsx
+++ app/_components/InfoComp.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useStoreContext } from "@/context/store";
 import { InfoIcon } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const InfoComp = () => {
   const { theme } = useStoreContext();
